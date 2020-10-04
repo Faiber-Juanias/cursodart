c931ac1ac3417bd83275ac282121cac80a4a3461
Faiber_Juanias <faiberjuanias001@gmail.com>
2020-10-03 21:33:43 -0500

    Base

---
+++ bin/cursodart.dart
@@ -0,0 +1,13 @@
+
+void main(List<String> arguments) {
+
+  List<String> listaUno;
+  listaUno = ["Negro","Rojo"];
+
+  listaUno.add("Amarillo");
+  listaUno.remove("Negro");
+
+  print(listaUno);
+
+}
+
